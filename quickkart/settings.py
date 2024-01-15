779643e614a25f276e141f376007cbc6d64d1c0b
Harshal A <harshal73gh@gmail.com>
2024-01-15 12:00:24 +0530

    added media

--- quickkart/settings.py
+++ quickkart/settings.py
@@ -125,6 +125,11 @@ STATICFILES_DIRS = [
     'quickkart/static',
 ]
 
+#media files config
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
--- quickkart/urls.py
+++ quickkart/urls.py
@@ -17,9 +17,13 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+
+from quickkart.settings import MEDIA_ROOT
 from .import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home,name='home'),
-]
+]+ static(settings.MEDIA_URL, document_root=MEDIA_ROOT)
