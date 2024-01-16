677eee4d3a17f8718c086458d09d6b65381005dd
Harshal A <harshal73gh@gmail.com>
2024-01-16 20:22:01 +0530

    add to cart and remove implemented

--- carts/views.py
+++ carts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from store.models import product
 from .models import Cart,Cartitem
 from django.http import HttpResponse
@@ -34,6 +34,29 @@ def add_cart(request, product_id):
         )
         cart_item.save()
     return redirect('cart')    
+
+
+def remove_cart(request,product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    Product = get_object_or_404(product,id=product_id)
+    cart_item = Cartitem.objects.get(Product=Product, cart=cart)
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+    return redirect('cart')        
+
+
+def remove_cart_item(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    Product = get_object_or_404(product, id=product_id)
+    cart_item = Cartitem.objects.get(Product=Product, cart=cart)
+    cart_item.delete()
+    return redirect('cart')
+
+
+
 
 def cart(request,total=0, quantity=0, cart_items=None):
     try:
